509fd2513cc5e8441073153a447ae3a73a7c67fd
dobi10 <dozohi1029@gmail.com>
2026-08-05 17:38:34 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME="codesphere-v1";
+
+
+const files=[
+
+"index.html",
+"style.css",
+"courses.html",
+"dashboard.html"
+
+];
+
+
+self.addEventListener(
+"install",
+event=>{
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache=>cache.addAll(files))
+
+);
+
+});
+
+
+
+self.addEventListener(
+"fetch",
+event=>{
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response=>{
+
+return response ||
+fetch(event.request);
+
+})
+
+);
+
+});
