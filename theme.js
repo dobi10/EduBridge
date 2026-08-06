5f4a745b88d9800902746536174ee84595f0887b
dobi10 <dozohi1029@gmail.com>
2026-08-07 01:14:26 +0300

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,25 @@
+const button =
+document.getElementById("themeToggle");
+
+
+button.onclick = ()=>{
+
+document.body.classList.toggle(
+"dark"
+);
+
+
+localStorage.setItem(
+"theme",
+document.body.classList.contains("dark")
+);
+
+};
+
+
+
+if(localStorage.getItem("theme") === "true"){
+
+document.body.classList.add("dark");
+
+}
